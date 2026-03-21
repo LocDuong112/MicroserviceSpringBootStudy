7023e594ee642a51fe7344381682fb482463a431
LocDuong <duongminhloc201@gmail.com>
2026-03-21 23:09:24 +0700

    Add inventory check for quantity and integration test

---
+++ inventory-service/src/main/resources/db/migration/V2__add_inventory.sql
@@ -0,0 +1,6 @@
+insert into t_inventory (quantity, sku_code)
+values (1000, 'Samsung_smart_screen_45_inch'),
+       (1500, 'Samsung_android_screen_45_inch'),
+       (20000, 'Apple_smart_screen_45_inch'),
+       (35000, 'Apple_CPU_M100'),
+       (1, 'banh_mi_Viet_Nam');
