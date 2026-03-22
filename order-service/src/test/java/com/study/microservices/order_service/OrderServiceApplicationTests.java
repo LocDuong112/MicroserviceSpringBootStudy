703c37e1832ac4cbceb8d317e2758319ed5f209b
LocDuong <duongminhloc201@gmail.com>
2026-03-22 22:16:36 +0700

    Add wiremock for test calling to other service

--- order-service/src/test/java/com/study/microservices/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/study/microservices/order_service/OrderServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.study.microservices.order_service;
 
+import com.study.microservices.order_service.stub.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.apache.http.HttpStatus;
 import org.hamcrest.Matchers;
@@ -8,11 +9,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.testcontainers.containers.MySQLContainer;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
 	@ServiceConnection
@@ -40,6 +43,8 @@ class OrderServiceApplicationTests {
 				    "quantity": 1
 				}
 				""";
+
+		InventoryClientStub.stubInventoryCall("banh_mi_Viet_Nam", 1);
 
 		var repsonse = RestAssured.given()
 				.contentType("application/json")
