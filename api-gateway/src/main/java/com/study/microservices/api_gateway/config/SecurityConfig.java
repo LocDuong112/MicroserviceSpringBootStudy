9066dcaf506521f9ecf433612c48087ab5f5f46c
LocDuong <duongminhloc201@gmail.com>
2026-03-24 12:07:01 +0700

    Refactor - not use OpenFeign to call internal service, now use Spring GetExchange, because OpenFeign not supported. Config to see all Swagger in Gateway.

--- api-gateway/src/main/java/com/study/microservices/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/study/microservices/api_gateway/config/SecurityConfig.java
@@ -9,10 +9,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    private  final String[] freeResourceUrl = {"/swagger-ui.html", "/swagger-ui/**", "/swagger-resources/**", "/v3/api-doc/**", "/api-doc/**", "/aggregate/**"};
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest()
-                .authenticated())
+        return httpSecurity.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(freeResourceUrl).permitAll()
+                        .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
