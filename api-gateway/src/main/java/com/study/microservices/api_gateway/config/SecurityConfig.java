49ee33cd4e2df2d3455dc3618a37da24806fa307
LocDuong <duongminhloc201@gmail.com>
2026-03-27 13:08:10 +0700

    Add cors config, frontend

--- api-gateway/src/main/java/com/study/microservices/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/study/microservices/api_gateway/config/SecurityConfig.java
@@ -13,9 +13,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(authorize -> authorize
+        return httpSecurity
+                .cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(freeResourceUrl).permitAll()
-                        .anyRequest().authenticated())
+                        .anyRequest().authenticated()
+                )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
